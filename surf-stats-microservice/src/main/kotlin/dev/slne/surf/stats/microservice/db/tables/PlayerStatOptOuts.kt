e2798f5e6127231a966a3f33a385c58499ee652a
Jan <jan.hoeck@gmx.net>
2026-07-22 21:25:14 +0200

    Change charuuid to nativeuuid

--- surf-stats-microservice/src/main/kotlin/dev/slne/surf/stats/microservice/db/tables/PlayerStatOptOuts.kt
+++ surf-stats-microservice/src/main/kotlin/dev/slne/surf/stats/microservice/db/tables/PlayerStatOptOuts.kt
@@ -1,11 +1,11 @@
 package dev.slne.surf.stats.microservice.db.tables
 
 import dev.slne.surf.api.core.messages.adventure.key
-import dev.slne.surf.database.columns.charUuid
+import dev.slne.surf.database.columns.nativeUuid
 import dev.slne.surf.database.libs.org.jetbrains.exposed.v1.core.Table
 
 object PlayerStatOptOuts : Table("player_stat_optouts") {
-    val playerUuid = charUuid("player_uuid")
+    val playerUuid = nativeUuid("player_uuid")
     val categoryName = varchar("category_name", 128)
         .transform({ key(it) }, { it.asString() })
         .references(StatCategoriesTable.name)
